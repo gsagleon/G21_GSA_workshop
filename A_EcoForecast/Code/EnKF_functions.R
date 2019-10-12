db42275d3128b7008b61a3a02fa102478579a301
jzwart <jzwart@usgs.gov>
2019-10-12 15:26:03 -0400

    updating forecasting workshop

--- A_EcoForecast/Code/EnKF_functions.R
+++ A_EcoForecast/Code/EnKF_functions.R
@@ -94,7 +94,7 @@ get_obs_matrix = function(obs_df, model_dates, n_step, n_states){
   obs_matrix = array(NA, dim = c(n_states, 1, n_step))
 
   for(j in obs_df_filtered$date_step){
-    obs_matrix[i, 1, j] = dplyr::filter(obs_df_filtered,
+    obs_matrix[1, 1, j] = dplyr::filter(obs_df_filtered,
                                         date_step == j) %>%
       pull(doc_lake)
   }
@@ -121,8 +121,8 @@ kalman_filter = function(Y, R, obs, H, n_en, cur_step){
   delta_Y = Y[ , cur_step, ] - matrix(rep(Y_mean, n_en), nrow = length(Y[ , 1, 1])) # difference in ensemble state/parameter and mean of all ensemble states/parameters
   
   ###### estimate Kalman gain #########
-  K = ((1 / (n_en - 1)) * delta_Y %*% t(delta_Y) %*% t(H[, , cur_step])) %*%
-    qr.solve(((1 / (n_en - 1)) * H[, , cur_step] %*% delta_Y %*% t(delta_Y) %*% t(H[, , cur_step]) + R[, , cur_step]))
+  K = ((1 / (n_en - 1)) * delta_Y %*% t(delta_Y) %*% matrix(t(H[, , cur_step]))) %*%
+    qr.solve(((1 / (n_en - 1)) * H[, , cur_step] %*% delta_Y %*% t(delta_Y) %*% matrix(t(H[, , cur_step])) + R[, , cur_step]))
   
   ###### update Y vector ######
   for(q in 1:n_en){
@@ -137,7 +137,7 @@ kalman_filter = function(Y, R, obs, H, n_en, cur_step){
 #'
 #' @param Y Y vector
 #' @param obs observation matrix
-initialize_Y = function(Y, obs, n_states_est, n_params_est, n_params_obs, n_step, n_en, state_sd, param_sd){
+initialize_Y = function(Y, obs, init_params, n_states_est, n_params_est, n_params_obs, n_step, n_en, state_sd, param_sd){
   
   # initializing states with earliest observations and parameters
   first_obs = coalesce(!!!lapply(seq_len(dim(obs)[3]), function(i){obs[,,i]})) %>% # turning array into list, then using coalesce to find first obs in each position.
@@ -145,18 +145,183 @@ initialize_Y = function(Y, obs, n_states_est, n_params_est, n_params_obs, n_step
   
   if(n_params_est > 0){
     ## update this later ***********************
-    first_params = rep(.5, n_params_est)
+    first_params = init_params
   }else{
     first_params = NULL
   }
   
-  Y[ , 1, ] = array(rnorm(n = n_en * (n_states_est + n_params_est),
-                          mean = c(first_obs, first_params),
-                          sd = c(state_sd, param_sd)),
+  Y[ , 1, ] = array(abs(rnorm(n = n_en * (n_states_est + n_params_est),
+                              mean = c(first_obs, first_params),
+                              sd = c(state_sd, param_sd))),
                     dim = c(c(n_states_est + n_params_est), n_en))
   
   return(Y)
 }
 
+
+#' matrix for holding driver data
+#'
+#' @param drivers_df dataframe which holds all the driver data 
+#' @param model_dates dates for model run 
+#' @param n_drivers number of model drivers 
+#' @param driver_colnames column names of the drivers in the driver dataframe 
+#' @param driver_cv coefficient of variation for each driver data 
+#' @param n_step number of model timesteps
+#' @param n_en number of ensembles
+get_drivers = function(drivers_df, model_dates, n_drivers, driver_colnames, driver_cv, n_step, n_en){
+  
+  drivers_filtered = drivers_df %>% 
+    dplyr::filter(as.Date(datetime) %in% model_dates)
+
+  drivers_out = array(NA, dim = c(n_step, n_drivers, n_en))
+  
+  for(i in 1:n_drivers){
+    for(j in 1:n_step){
+      drivers_out[j,i,] = rnorm(n = n_en, 
+                               mean = as.numeric(drivers_filtered[j, driver_colnames[i]]),
+                               sd = as.numeric(driver_cv[i] * drivers_filtered[j, driver_colnames[i]]))
+    }
+  }
+  
+  return(drivers_out) 
+}
+
+
+#' wrapper for running EnKF 
+#' 
+#' @param n_en number of model ensembles 
+#' @param start start date of model run 
+#' @param stop date of model run
+#' @param time_step model time step, defaults to days 
+#' @param obs_file observation file 
+#' @param driver_file driver data file 
+#' @param n_states_est number of states we're estimating 
+#' @param n_params_est number of parameters we're estimating 
+#' @param n_params_obs number of parameters for which we have observations 
+#' @param decay_init initial decay rate of DOC 
+#' @param obs_cv coefficient of variation of observations 
+#' @param param_cv coefficient of variation of parameters 
+#' @param driver_cv coefficient of variation of driver data for DOC Load, Discharge out, and Lake volume, respectively 
+#' @param init_cond_cv initial condition CV (what we're )
+EnKF = function(n_en = 100, 
+                start,
+                stop, 
+                time_step = 'days', 
+                obs_file = 'A_EcoForecast/Data/lake_c_data.rds',
+                driver_file = 'A_EcoForecast/Data/lake_c_data.rds',
+                n_states_est = 1, 
+                n_params_est = 1,
+                n_params_obs = 0, 
+                decay_init = 0.005, 
+                obs_cv = 0.1,
+                param_cv = 0.25,
+                driver_cv = c(0.2, 0.2, 0.2),
+                init_cond_cv = 0.1){
+  
+  
+  n_en = n_en
+  start = as.Date(start)
+  stop = as.Date(stop)
+  time_step = 'days' 
+  dates = get_model_dates(model_start = start, model_stop = stop, time_step = time_step)
+  n_step = length(dates)
+  
+  # get observation matrix
+  obs_df = readRDS(obs_file) %>% 
+    select(datetime, doc_lake) 
+  
+  drivers_df = readRDS(driver_file) %>% 
+    select(datetime, doc_load, water_out, lake_vol) 
+  
+  n_states_est = n_states_est # number of states we're estimating 
+  
+  n_params_est = n_params_est # number of parameters we're calibrating
+  
+  n_params_obs = n_params_obs # number of parameters for which we have observations
+  
+  decay_init = decay_init # Initial estimate of DOC decay rate day^-1 
+  
+  doc_init = obs_df$doc_lake[min(which(!is.na(obs_df$doc_lake)))]
+  
+  state_cv = obs_cv #coefficient of variation of DOC observations 
+  state_sd = state_cv * doc_init
+  init_cond_sd = init_cond_cv * doc_init
+  
+  param_cv = param_cv #coefficient of variation of DOC decay 
+  param_sd = param_cv * decay_init
+  
+  # driver data coefficient of variation for DOC Load, Discharge out, and Lake volume, respectively 
+  driver_cv = driver_cv 
+  
+  
+  # setting up matrices
+  # observations as matrix
+  obs = get_obs_matrix(obs_df = obs_df,
+                       model_dates = dates,
+                       n_step = n_step,
+                       n_states = n_states_est)
+  
+  # Y vector for storing state / param estimates and updates
+  Y = get_Y_vector(n_states = n_states_est,
+                   n_params_est = n_params_est,
+                   n_step = n_step,
+                   n_en = n_en)
+  
+  # observation error matrix
+  R = get_obs_error_matrix(n_states = n_states_est,
+                           n_params_obs = n_params_obs,
+                           n_step = n_step,
+                           state_sd = state_sd,
+                           param_sd = param_sd)
+  
+  # observation identity matrix
+  H = get_obs_id_matrix(n_states = n_states_est,
+                        n_params_obs = n_params_obs,
+                        n_params_est = n_params_est,
+                        n_step = n_step,
+                        obs = obs)
+  
+  # initialize Y vector
+  Y = initialize_Y(Y = Y, obs = obs, init_params = decay_init, n_states_est = n_states_est,
+                   n_params_est = n_params_est, n_params_obs = n_params_obs,
+                   n_step = n_step, n_en = n_en, state_sd = init_cond_sd, param_sd = param_sd)
+  
+  # get driver data with uncertainty - dim = c(n_step, driver, n_en) 
+  drivers = get_drivers(drivers_df = drivers_df, 
+                        model_dates = dates,
+                        n_drivers = 3, 
+                        driver_colnames = c('doc_load', 'water_out', 'lake_vol'), 
+                        driver_cv = driver_cv, 
+                        n_step = n_step, 
+                        n_en = n_en) 
+  
+  # start modeling
+  for(t in 2:n_step){
+    for(n in 1:n_en){
+      
+      # run model; 
+      model_output = predict_lake_doc(doc_load = drivers[t-1, 1, n], 
+                                      doc_lake = Y[1, t-1, n], 
+                                      lake_vol = drivers[t-1, 3, n], 
+                                      water_out = drivers[t-1, 2, n],
+                                      decay = Y[2, t-1, n])
+      
+      Y[1 , t, n] = model_output$doc_predict # store in Y vector
+      Y[2 , t, n] = model_output$decay
+    }
+    # check if there are any observations to assimilate 
+    if(any(!is.na(obs[ , , t]))){
+      Y = kalman_filter(Y = Y,
+                        R = R,
+                        obs = obs,
+                        H = H,
+                        n_en = n_en,
+                        cur_step = t) # updating params / states if obs available
+    }
+  }
+  out = list(Y = Y, dates = dates, drivers_df = drivers_df, R = R, obs = obs, state_sd = state_sd)
+  
+  return(out)
+}
 
 
--- A_EcoForecast/Code/run_forecast.R
+++ A_EcoForecast/Code/run_forecast.R
@@ -4,98 +4,35 @@ library(dplyr)
 source('A_EcoForecast/Code/EnKF_functions.R')
 source('A_EcoForecast/Code/lake_doc_model.R')
 
-# get model start, stop, full dates, and n_steps
-n_en = 100
-start = as.Date('2014-06-04')
-stop = as.Date('2014-09-20')
-time_step = 'days' 
-dates = get_model_dates(model_start = start, model_stop = stop, time_step = time_step)
-n_step = length(dates)
+n_en = 100 # how many ensembles 
 
-# get observation matrix
-obs_df = readRDS('A_EcoForecast/Data/lake_c_data.rds') %>% 
-  select(datetime, doc_lake) 
+est_out = EnKF(n_en = n_en, 
+           start = '2014-06-04', 
+           stop = '2014-09-20')
 
-n_states_est = 1 # number of states we're estimating 
-
-n_params_est = 1 # number of parameters we're calibrating
-
-n_params_obs = 0 # number of parameters for which we have observations
-
-# temp for now, add as input later *****************************
-state_sd = rep(.5, n_states_est)
-
-param_sd = rep(.2, n_params_obs)
-
-# setting up matrices
-# observations as matrix
-obs = get_obs_matrix(obs_df = obs_df,
-                     model_dates = dates,
-                     n_step = n_step,
-                     n_states = n_states_est)
-
-
-# Y vector for storing state / param estimates and updates
-Y = get_Y_vector(n_states = n_states_est,
-                 n_params_est = n_params_est,
-                 n_step = n_step,
-                 n_en = n_en)
-
-# observation error matrix
-R = get_obs_error_matrix(n_states = n_states_est,
-                         n_params_obs = n_params_obs,
-                         n_step = n_step,
-                         state_sd = state_sd,
-                         param_sd = param_sd)
-
-# observation identity matrix
-H = get_obs_id_matrix(n_states = n_states_est,
-                      n_params_obs = n_params_obs,
-                      n_params_est = n_params_est,
-                      n_step = n_step,
-                      obs = obs)
-
-# use this later for organizing obs error matrix *******************************
-n_states_obs = 1 # number of states we're updating; will be dependent on obs
-
-# initialize Y vector
-Y = initialize_Y(Y = Y, obs = obs, n_states_est = n_states_est,
-                 n_params_est = n_params_est, n_params_obs = n_params_obs,
-                 n_step = n_step, n_en = n_en, state_sd = state_sd, param_sd = param_sd)
-
-drivers = data2[, ]
-
-# start modeling
-for(t in 2:n_step){
-  for(n in 1:n_en){
-    
-    # run model; 
-    model_output = predict_lake_doc(doc_load = data2$doc_load[t-1], 
-                                    doc_lake = Y[1, t-1, n], 
-                                    lake_vol = data2$lake_vol[t-1], 
-                                    water_out = data2$water_out[t-1],
-                                    decay = Y[2, t-1, n])
-    
-    Y[1 , t, n] = model_output$doc_predict # store in Y vector
-    Y[2 , t, n] = model_output$decay
-  }
-  if(any(!is.na(obs[ , , t]))){
-    Y = kalman_filter(Y = Y,
-                      R = R,
-                      obs = obs,
-                      H = H,
-                      n_en = n_en,
-                      cur_step = t) # updating params / states if obs available
-  }
-  # update states / params for model config
+# plotting 
+mean_doc_est = apply(est_out$Y[1,,], 1, FUN = mean)
+plot(mean_doc_est / est_out$drivers_df$lake_vol * 12 ~ est_out$dates, type ='l', 
+     ylim = range(est_out$Y[1,,] / est_out$drivers_df$lake_vol * 12),
+     col = 'grey', ylab = 'DOC (mg L-1)', xlab = '')
+for(i in 2:n_en){
+  lines(est_out$Y[1,,i] / est_out$drivers_df$lake_vol * 12 ~ est_out$dates, 
+        col = 'grey')
 }
+lines(mean_doc_est / est_out$drivers_df$lake_vol * 12 ~ est_out$dates, col = 'black', lwd =2 )
+points(est_out$obs[1,,] / est_out$drivers_df$lake_vol * 12 ~ est_out$dates, pch = 16, col = 'red')
+arrows(est_out$dates, est_out$obs[1,,] / est_out$drivers_df$lake_vol * 12 - est_out$state_sd / est_out$drivers_df$lake_vol * 12, 
+       est_out$dates, est_out$obs[1,,] / est_out$drivers_df$lake_vol * 12 + est_out$state_sd / est_out$drivers_df$lake_vol * 12, 
+       code = 3, length = 0.1, angle = 90, col = 'red')
 
-out = list(Y = Y, dates = dates, obs = obs, R = R, model_locations = model_locations)
-
-
-
-
-
+mean_decay_est = apply(est_out$Y[2,,], 1, FUN = mean)
+plot(mean_decay_est ~ est_out$dates, type ='l', 
+     ylim = range(est_out$Y[2,,]),
+     col = 'grey', ylab = 'DOC Decay (day^-1)', xlab ='')
+for(i in 2:n_en){
+  lines(est_out$Y[2,,i] ~ est_out$dates, col = 'grey')
+}
+lines(mean_decay_est ~ est_out$dates, col = 'black', lwd = 2)
 
 
 
